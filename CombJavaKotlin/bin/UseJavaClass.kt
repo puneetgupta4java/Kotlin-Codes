caf464e1f4ecc8840327bafebfc08ed8e15590be
Puneet Gupta <puneetgupta620@gmail.com>
2017-05-28 01:44:12 +0530

    Add files via upload
    Combined code of java and kotlin

---
+++ CombJavaKotlin/bin/UseJavaClass.kt
@@ -0,0 +1,11 @@
+	
+	fun main(args : Array<String>){
+		
+		var usevariable = POJO()
+		usevariable.name = "puneet"
+		
+		println(usevariable.name)
+		
+	
+	
+}
---
+++ CombJavaKotlin/src/POJO.java
@@ -0,0 +1,14 @@
+
+public class POJO {
+
+	private String name;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+}
